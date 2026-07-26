bb225779b1c32122986b40b345f1460859d4df2b
Hydroponic-Systems-ps <ppp112606@gmail.com>
2026-07-26 23:57:53 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,16 +1,25 @@
 async function loadData() {
-  const { data, error } = await supabaseClient
+  const { data, error } = await supabase
     .from("sensor_readings")
     .select("*")
-    .order("reading_time", { ascending: false })
+    .order("created_at", { ascending: false })
     .limit(1);
 
   if (error) {
-    console.error(error);
+    console.log(error);
     return;
   }
 
-  console.log(data);
+  if (data.length > 0) {
+    const row = data[0];
+
+    document.getElementById("ph").innerText = row.ph;
+    document.getElementById("ec").innerText = row.ec + " mS/cm";
+    document.getElementById("ppm").innerText = row.ppm;
+    document.getElementById("temp").innerText = row.temperature + " °C";
+    document.getElementById("level").innerText = row.water_level + " L";
+  }
 }
 
 loadData();
+setInterval(loadData, 5000);
