c17d7e16f672834cee2ef07d4c1653021343c866
Hydroponic-Systems-ps <ppp112606@gmail.com>
2026-07-27 00:15:16 +0530

    Update dashboard.js
    some error fix

--- dashboard.js
+++ dashboard.js
@@ -1,24 +1,29 @@
 async function loadData() {
-  const { data, error } = await supabase
+
+  const { data, error } = await supabaseClient
     .from("sensor_readings")
     .select("*")
     .order("reading_time", { ascending: false })
     .limit(1);
 
   if (error) {
-    console.log(error);
+    console.error(error);
+    alert(error.message);
     return;
   }
 
-  if (data.length > 0) {
-    const row = data[0];
-
-    document.getElementById("ph").innerText = row.ph;
-    document.getElementById("ec").innerText = row.ec + " mS/cm";
-    document.getElementById("ppm").innerText = row.ppm;
-    document.getElementById("temp").innerText = row.water_temp + " °C";
-    document.getElementById("level").innerText = row.water_level + " L";
+  if (!data || data.length === 0) {
+    alert("No data found in sensor_readings table");
+    return;
   }
+
+  const row = data[0];
+
+  document.getElementById("ph").innerText = row.ph;
+  document.getElementById("ec").innerText = row.ec + " mS/cm";
+  document.getElementById("ppm").innerText = row.ppm;
+  document.getElementById("temp").innerText = row.water_temp + " °C";
+  document.getElementById("level").innerText = row.water_level + " L";
 }
 
 loadData();
